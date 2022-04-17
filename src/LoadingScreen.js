85c727bae683854e7d0bf15637646516212dd73c
vmanicse <vmani.cs17@outlook.com>
2022-04-17 22:39:01 +0530

    code updated

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -1,18 +1,25 @@
 import Skeleton from "react-loading-skeleton";
 import 'react-loading-skeleton/dist/skeleton.css';
+import './LoadingScreen.css';
 
 function LoadingScreen() {
 
     function getPlaceholders() {
         return(
-            <div style={{display:"flex", justifyContent: "space-around", flexWrap:"wrap", width:"85%", margin:"auto"}}>
-                <div style={{width:"33%", margin:"auto"}}>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-around",
+                flexWrap: "wrap",
+                width: "85%",
+                margin: "auto"
+            }}>
+                <div className="skeleton">
                     <Skeleton count={10} baseColor="#c0c9cf" height="10px" />
                 </div>
-                <div style={{width:"33%", margin:"auto"}}>
+                <div className="skeleton">
                     <Skeleton count={10} baseColor="#c0c9cf" height="10px" />
                 </div>
-                <div style={{width:"33%", margin:"auto"}}>
+                <div className="skeleton">
                     <Skeleton count={10} baseColor="#c0c9cf" height="10px" />
                 </div>
             </div>
